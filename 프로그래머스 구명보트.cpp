938764f5ee8467785defd605a10c78d7eeda7592
Takmin <jtm0609@naver.com>
2020-10-29 22:50:25 +0900

    프로그래머스 구명보트+ H-index 풀이

---
+++ 프로그래머스 구명보트.cpp
@@ -0,0 +1,26 @@
+//문제: 구명보트 LV2
+//알고리즘: 그리디 알고리즘
+//참고: https://m.blog.naver.com/tkddn0928/221790401154
+
+#include <string>
+#include <vector>
+#include<algorithm>
+
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+	int answer = 0;
+	sort(people.begin(), people.end());
+	int head = 0, tail = people.size() - 1;
+	//제일 무게가작은놈을 기준으로 무게가 젤큰놈이랑비교해나간다.
+	while (head <= tail) {
+		if (people[head] + people[tail] <= limit) {
+			head++; tail--;
+		}
+		else tail--;
+		answer++;
+	}
+
+	return answer;
+}
